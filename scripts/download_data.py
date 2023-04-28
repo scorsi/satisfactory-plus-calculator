a71626d3a7a4bbfaef89cf7d3cab4edc8b3cc160
scorsi <sylvain.corsini@protonmail.com>
2023-04-28 02:10:34 +0200

    feat(scripts): add download_data script

---
+++ scripts/download_data.py
@@ -0,0 +1,12 @@
+import requests
+
+
+def download_data():
+    r = requests.get("http://localhost:8080/getSchematics")
+    schematics = r.json()
+    with open("data/schematics.json", "w") as f:
+        f.write(r.text)
+
+
+if __name__ == '__main__':
+    download_data()
