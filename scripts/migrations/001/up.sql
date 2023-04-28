a70e123fe17a6bab185e470efabeda8a277048c1
scorsi <sylvain.corsini@protonmail.com>
2023-04-28 17:42:09 +0200

    feat(migrations): add migration 001

--- scripts/migrations/001/up.sql
+++ scripts/migrations/001/up.sql
@@ -1,18 +1,26 @@
 CREATE TABLE items (
-    item_id VARCHAR(255) PRIMARY KEY,
+    id VARCHAR(255) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
+    categories JSON NOT NULL,
     produced_in JSON NOT NULL,
     consumed_in JSON NOT NULL
 );
 
 CREATE TABLE recipes (
-    recipe_id VARCHAR(255) PRIMARY KEY,
+    id VARCHAR(255) PRIMARY KEY,
     schematic_id VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
-    category VARCHAR(255) NOT NULL,
+    categories JSON NOT NULL,
     ingredients JSON NOT NULL,
     products JSON NOT NULL,
     buildings JSON NOT NULL,
-    duration_manual INTEGER NOT NULL,
-    duration_manufacturing INTEGER NOT NULL
+    duration JSON NOT NULL
+);
+
+CREATE TABLE schematics (
+    id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    tier INT NOT NULL,
+    categories JSON NOT NULL,
+    recipes_ids JSON NOT NULL
 );
