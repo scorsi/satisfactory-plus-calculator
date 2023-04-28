55d0cd0e570c8ecb0d89f890b1448019aead64b0
scorsi <sylvain.corsini@protonmail.com>
2023-04-28 23:31:05 +0200

    feat(web): add empty profile page

---
+++ web/src/pages/profile/index.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+const ProfileView: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>SF+ Calculator :: Profile</title>
+      </Head>
+      <h1>Profile</h1>
+    </>
+  )
+}
+
+export default ProfileView;
