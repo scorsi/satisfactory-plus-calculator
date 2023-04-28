9c50b522114585c48243849e8507852164d9ea66
scorsi <sylvain.corsini@protonmail.com>
2023-04-28 02:09:13 +0200

    feat(scripts): add python venv

--- scripts/cli.ts
+++ scripts/cli.ts
@@ -14,80 +14,84 @@ dotenv.config({
   path: path.join(rootDir, ".env")
 });
 
+async function migrateUp() {
+  const db = new Kysely({
+    dialect: new PlanetScaleDialect({
+      host: process.env.PLANETSCALE_DATABASE_HOST,
+      username: process.env.PLANETSCALE_DATABASE_USERNAME,
+      password: process.env.PLANETSCALE_DATABASE_PASSWORD
+    })
+  });
+
+  const migrator = new Migrator({
+    db,
+    provider: new FileMigrationProvider({
+      fs,
+      path,
+      migrationFolder: path.join(rootDir, "migrations")
+    })
+  });
+
+  const { error, results } = await migrator.migrateToLatest();
+
+  results?.forEach((it) => {
+    if (it.status === "Success") {
+      console.log(`migration "${it.migrationName}" was executed successfully`);
+    } else if (it.status === "Error") {
+      console.error(`failed to execute migration "${it.migrationName}"`);
+    }
+  });
+
+  if (error) {
+    console.error("failed to migrate");
+    console.error(error);
+    process.exit(1);
+  }
+
+  await db.destroy();
+}
+
+async function migrateDown() {
+  const db = new Kysely({
+    dialect: new PlanetScaleDialect({
+      host: process.env.PLANETSCALE_DATABASE_HOST,
+      username: process.env.PLANETSCALE_DATABASE_USERNAME,
+      password: process.env.PLANETSCALE_DATABASE_PASSWORD
+    })
+  });
+
+  const migrator = new Migrator({
+    db,
+    provider: new FileMigrationProvider({
+      fs,
+      path,
+      migrationFolder: path.join(rootDir, "migrations")
+    })
+  });
+
+  const { error, results } = await migrator.migrateTo(NO_MIGRATIONS);
+
+  results?.forEach((it) => {
+    if (it.status === "Success") {
+      console.log(`migration "${it.migrationName}" was executed successfully`);
+    } else if (it.status === "Error") {
+      console.error(`failed to execute migration "${it.migrationName}"`);
+    }
+  });
+
+  if (error) {
+    console.error("failed to migrate");
+    console.error(error);
+    process.exit(1);
+  }
+
+  await db.destroy();
+}
+
 void yargs(process.argv.slice(2))
   .usage("$0 <cmd> [args]")
   .scriptName("migrator")
-  .command("migrate:up", "migrate to latest version", {}, async () => {
-    const db = new Kysely({
-      dialect: new PlanetScaleDialect({
-        host: process.env.PLANETSCALE_DATABASE_HOST,
-        username: process.env.PLANETSCALE_DATABASE_USERNAME,
-        password: process.env.PLANETSCALE_DATABASE_PASSWORD
-      })
-    });
-
-    const migrator = new Migrator({
-      db,
-      provider: new FileMigrationProvider({
-        fs,
-        path,
-        migrationFolder: path.join(rootDir, "migrations")
-      })
-    });
-
-    const { error, results } = await migrator.migrateToLatest();
-
-    results?.forEach((it) => {
-      if (it.status === "Success") {
-        console.log(`migration "${it.migrationName}" was executed successfully`);
-      } else if (it.status === "Error") {
-        console.error(`failed to execute migration "${it.migrationName}"`);
-      }
-    });
-
-    if (error) {
-      console.error("failed to migrate");
-      console.error(error);
-      process.exit(1);
-    }
-
-    await db.destroy();
-  })
-  .command("migrate:down", "drop all migration", {}, async () => {
-    const db = new Kysely({
-      dialect: new PlanetScaleDialect({
-        host: process.env.PLANETSCALE_DATABASE_HOST,
-        username: process.env.PLANETSCALE_DATABASE_USERNAME,
-        password: process.env.PLANETSCALE_DATABASE_PASSWORD
-      })
-    });
-
-    const migrator = new Migrator({
-      db,
-      provider: new FileMigrationProvider({
-        fs,
-        path,
-        migrationFolder: path.join(rootDir, "migrations")
-      })
-    });
-
-      const { error, results } = await migrator.migrateTo(NO_MIGRATIONS);
-
-      results?.forEach((it) => {
-        if (it.status === "Success") {
-          console.log(`migration "${it.migrationName}" was executed successfully`);
-        } else if (it.status === "Error") {
-          console.error(`failed to execute migration "${it.migrationName}"`);
-        }
-      });
-
-      if (error) {
-        console.error("failed to migrate");
-        console.error(error);
-        process.exit(1);
-      }
-
-      await db.destroy();
-  })
+  .command("migrate:up", "migrate to latest version", {}, migrateUp)
+  .command("migrate:down", "drop all migration", {}, migrateDown)
   .help()
   .argv;
